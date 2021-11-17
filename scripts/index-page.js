5876e94a78549b9bb5f3aea76b5df705d6289f2f
Fadya J <f.joudh95@gmail.com>
2021-11-16 18:45:41 -0800

    file set up& sprint2

---
+++ scripts/index-page.js
@@ -0,0 +1,10 @@
+let comments = [{}, {}, {}]; // default comment
+
+function insertComment(comment) {
+  const commentSecrion = document.find; // comment section
+  commentSecrion.add``;
+}
+
+for (let index = 0; index < comments.length; index++) {
+  insertComment(comments[index]);
+}
